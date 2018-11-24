54466ce7e4a65e2327fcf6152ed35bc718847f74
urubu <urubu@urubus-MacBook-Air.local>
2018-11-24 15:08:49 +0100

    Changed debug to false.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#!up7=f+!egeb(^m1lkps*)^hoylfqxzs27f27g^(m7lel2s83'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
